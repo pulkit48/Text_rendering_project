cff3eae029e5d67d44d5a1ab1bf3ff72f6194ec4
pulkit48 <135384814+pulkit48@users.noreply.github.com>
2025-11-02 00:04:16 +0530

    Update random_code.py

--- random_code.py
+++ random_code.py
@@ -28,3 +28,23 @@ res = model.infer(tokenizer, prompt=prompt, image_file=image_file, output_path =
 # dtype = torch.bfloat16 if device == "cuda" else torch.float32
 
 # model = model.eval().to(device, dtype=dtype)
+
+
+
+
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import requests
+from PIL import Image
+
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
+model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
+
+# load image from the IAM dataset
+url = "https://fki.tic.heia-fr.ch/static/img/a01-122-02.jpg"
+image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+
+pixel_values = processor(image, return_tensors="pt").pixel_values
+generated_ids = model.generate(pixel_values)
+
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(generated_text)
